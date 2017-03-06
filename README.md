999e6bc2b0234c2a6601f292a1546bb2708cdaa7
villehar <ville.harjunen@helsinki.fi>
2017-03-06 18:30:43 +0200

    final data wrangling

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
 # IODS-final
 
-This repository is my final assignment of the IODS course. The full report of the assignment can be seen via this link https://villehar.github.io/IODS-final/
+This repository is for my final assignment of the IODS course. The full report can be seen via this link https://villehar.github.io/IODS-final/
 
--- create_ultima.R
+++ create_ultima.R
@@ -27,8 +27,52 @@ names(raw.data)[20] <- "no_problem"
 # Glimpsing the resulting data set
 glimpse(raw.data)
 
-#cahnging the variable class of the filter variables to logical
+#changing the variable class of the binary variables to logical
 names(raw.data)
-cols <- c(13, 17, 19, 20)
-raw.data[,cols] <- lapply(raw.data[,cols], function(x) as.logical(raw.data[,cols]))
-?lapply
+cols <- c(13, 17, 19, 20, 14)
+raw.data[,cols] <- raw.data[,cols] == 1
+str(raw.data)
+
+head(raw.data)
+#Filtering out the trials where the participant was the receiver
+resp.data <- filter(raw.data, myrole == "responder")
+str(resp.data)
+
+# create a filter for pp's with too many acceptances
+##staring with creating a aggregated data set with average acceptances per subject
+agg.accept <- aggregate(resp.data, by=list(resp.data$pp), FUN=mean)
+
+##selecting only the pp index and acceptances
+agg.accept <- select(agg.accept, pp, accept)
+
+#changing the name of 
+names(agg.accept)[2]<-"accept.prop"
+str(agg.accept)
+
+resp.data2 <- inner_join(resp.data, agg.accept, by = "pp")
+str(resp.data2)
+
+resp.data2 <- mutate(resp.data2, over90 = (accept.prop < .90))
+str(resp.data2)
+
+#defining the filtered data set. Participants with too high acceptance rate (90%), who knew the ultimatum game beforehand, and had problems in EEG measurement were removed
+#also the trials with very short and long reaction times were removed
+resp.data3 <- filter(resp.data2, over90 == TRUE, no_problem == TRUE, notknow_ult == TRUE, RT < 5000, ECGFILT == TRUE)
+str(resp.data3)
+
+write.csv(resp.data3, "responder.csv")
+
+library(GGally)
+library(tidyr)
+library(ggplot2)
+
+# A "block" variable was then created based on the trial variable
+resp.data3$Block <-cut(resp.data3$trial, c(1,150,300,450,600))
+ggplot(resp.data3, aes(x = RT, col = Block)) + geom_histogram(bins = 100)
+m <- glm(accept ~ Fairness + emo + touch, data = resp.data3, family = "binomial")
+summary(m)
+OR <- coef(m) %>% exp
+CI <- confint(m) %>% exp
+cbind(OR, CI)
+
+summary(resp.data3)
